ff70ac4eff6078d538f9ffcd6c3ac7dcc4a4b6bf
Sandrita41 <44031406+Sandrita41@users.noreply.github.com>
2021-07-22 22:30:16 +0300

    js updates + button updates

--- js/home.js
+++ js/home.js
@@ -1,14 +1,13 @@
 /* Header Area Start */
 window.onscroll = function() { myFunction() };
 var header = document.getElementById("header");
-var button = document.getElementById("download_sticky");
 var sticky = header.offsetTop;
 
 function myFunction() {
     if (window.pageYOffset > sticky) {
-        header.classList.add("sticky") & button.classList.add("download_sticky");
+        header.classList.add("sticky");
     } else {
-        header.classList.remove("sticky") & button.classList.add("download_sticky");
+        header.classList.remove("sticky");
     }
 }
 /* Header Area End */
